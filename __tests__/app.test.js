56397666e192e0f24bc5ded9195fc8802dca80ce
King Kong <king-kong@gmail.com>
2022-11-16 15:28:34 -0800

    authorize.js and 1st test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,21 +1,45 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 // const UserService = require('../lib/services/UserService');
 
-// const dumbUser = {
-//   email: 'Hello@example.com',
-//   password: 'SECRET PASSWORD',
+const dumbUser = {
+  email: 'Hello@example.com',
+  password: 'SECRET PASSWORD',
+};
+
+// const registerAndLogin = async (userProps = {}) => {
+//   const password = userProps.password ?? dumbUser.password;
+
+//   // Create an "agent" that gives us the ability
+//   // to store cookies between requests in a test
+//   const agent = request.agent(app);
+
+//   // Create a user to sign in with
+//   const user = await UserService.create({ ...dumbUser, ...userProps });
+
+//   // ...then sign in
+//   const { email } = user;
+//   await agent.post('/api/v1/users/sessions').send({ email, password });
+//   return [agent, user];
 // };
 
-describe('', () => {
+describe('top-secret-routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('POST { email, password } to /api/v1/users creates a new user', async () => {
+    const res = await (await request(app).post('/api/v1/users')).send(dumbUser);
+    const { email } = dumbUser;
+
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
   });
+
   afterAll(() => {
     pool.end();
   });
