ef9bd435c21cf0d99404c3d4e00771ae06518b28
King Kong <king-kong@gmail.com>
2022-11-16 15:56:58 -0800

    test, model, controller for creating a new user

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -9,29 +9,13 @@ const dumbUser = {
   password: 'SECRET PASSWORD',
 };
 
-// const registerAndLogin = async (userProps = {}) => {
-//   const password = userProps.password ?? dumbUser.password;
-
-//   // Create an "agent" that gives us the ability
-//   // to store cookies between requests in a test
-//   const agent = request.agent(app);
-
-//   // Create a user to sign in with
-//   const user = await UserService.create({ ...dumbUser, ...userProps });
-
-//   // ...then sign in
-//   const { email } = user;
-//   await agent.post('/api/v1/users/sessions').send({ email, password });
-//   return [agent, user];
-// };
-
 describe('top-secret-routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('POST { email, password } to /api/v1/users creates a new user', async () => {
-    const res = await (await request(app).post('/api/v1/users')).send(dumbUser);
+  it('POST  email, password  to /api/v1/users creates a new user', async () => {
+    const res = await request(app).post('/api/v1/users').send(dumbUser);
     const { email } = dumbUser;
 
     expect(res.body).toEqual({
