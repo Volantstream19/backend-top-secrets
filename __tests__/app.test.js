7093b9354cb64f7694e7061fa4abebf0894261c3
King Kong <king-kong@gmail.com>
2022-11-18 10:32:17 -0800

    delete - test, controller

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService.js');
 // const UserService = require('../lib/services/UserService');
 
 const dumbUser = {
@@ -42,6 +43,16 @@ describe('top-secret-routes', () => {
       password: 'SECRET PASSWORD',
     });
     expect(res.status).toEqual(200);
+  });
+
+  it('DELETE /api/v1/sessions signs out a user', async () => {
+    const agent = request.agent(app);
+    await UserService.create({ ...dumbUser });
+    await agent
+      .post('/api/v1/users/sessions')
+      .send({ email: 'Hello@example.com', password: 'SECRET PASSWORD' });
+    const resp = await agent.delete('/api/v1/users/sessions');
+    expect(resp.status).toBe(204);
   });
 });
 
